c0c70395ba685fde49a4e0e5f77190aa0fb35e70
bananvyhe <loadonden@yahoo.com>
2017-06-23 23:35:07 +0500

    lost gridsystem added

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,30 @@ var autoprefixer = require('autoprefixer');
 var precss = require('precss');
 var hamster = require('postcss-hamster');
 var livereload = require('gulp-livereload');
+var lost = require('lost');
  
-
 gulp.task('css', function () {
-   var plugins = [
-        precss({
-                "lookup": false
-        }),
-        hamster(),
-        autoprefixer({browsers: ["> 0.5%"]})
-
-        
-    ];
+  var plugins = [
+    precss({
+      "lookup": false
+    }),
+    postcssgulp,
+    hamster(),
+    lost(),
+    autoprefixer({browsers: ["> 0.5%"]})
+  ];
   return gulp.src('app/assets/stylesheets/postcss/application.css')
-    
-    .pipe(sourcemaps.init())
-    .pipe(postcssgulp(plugins))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('app/assets/stylesheets'))
-    .pipe(livereload());
-     
+   .pipe(sourcemaps.init())
+   .pipe(postcssgulp(plugins))
+   .pipe(sourcemaps.write('.'))
+   .pipe(gulp.dest('app/assets/stylesheets'))
+   .pipe(livereload());
 });
 
 gulp.task('html', function () {
    return gulp.src('app/views/indexpage/index.html.erb')
     .pipe(gulp.dest(''))
     .pipe(livereload());
-     
 });
 
 gulp.task('watch', function () {
